46820de14ff131f5104842b2c00796a680d00cb8
Andrew Roth <roth.andy@gmail.com>
2020-11-06 12:57:14 -0500

    Fixes to example and other stability adjustments (#3)

--- versions.tf
+++ versions.tf
@@ -1,22 +1,22 @@
 terraform {
-  required_version = ">= 0.13.0, < 0.14.0"
+  required_version = ">= 0.13.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0.0, < 3.0.0"
+      version = ">= 2.0.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = ">= 1.0.0, < 2.0.0"
+      version = ">= 1.0.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 2.0.0, < 3.0.0"
+      version = ">= 2.0.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 2.0.0, < 3.0.0"
+      version = ">= 2.0.0"
     }
   }
 }
