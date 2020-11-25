81f009c66608026b282a55cc598ad5cf1468acee
Aaron Miller <aaron.c.miller@saic.com>
2020-11-25 10:23:50 -0600

    Added token to gitlab for jenkins

--- gitlab.tf
+++ gitlab.tf
@@ -45,3 +45,21 @@ resource "null_resource" "add_jenkins_to_gitlab_applications" {
     null_resource.set_gitlab_personal_access_token
   ]
 }
+
+resource "null_resource" "set_api_token_for_jenkins" {
+  triggers = {
+    uuid = uuid()
+  }
+  provisioner "local-exec" {
+    command = "kubectl exec -n gitlab -c task-runner $(kubectl get pod -n gitlab -l \"app=task-runner\" -o jsonpath='{.items[0].metadata.name}') -- gitlab-rails runner 'user = User.find_by_username(\"'\"$GITLAB_USER\"'\"); tokens = user.personal_access_tokens; token = tokens.find_by(name: \"'\"$TOKEN_NAME\"'\"); token = user.personal_access_tokens.create(scopes: [:api, :sudo], name: \"'\"$TOKEN_NAME\"'\") unless token.present?; token.set_token(\"'\"$TOKEN_VALUE\"'\"); token.save!'"
+    environment = {
+      GITLAB_USER = "root"
+      KUBECONFIG  = abspath(local_file.kubeconfig.filename)
+      TOKEN_NAME  = "jenkins"
+      TOKEN_VALUE = random_password.gitlab_api_token.result
+    }
+  }
+  depends_on = [
+    null_resource.add_jenkins_to_gitlab_applications
+  ]
+}
