38e1c627c904a64d54438b5866eb23094c17e1af
Aaron Miller <32234519+ac-miller@users.noreply.github.com>
2021-01-11 18:55:18 -0600

    Stability Fix and Tagging AWS Resources (#14)
    * Adding tags to AWS resources
    
    * Unpin git plugin to allow latest to install
    
    * Removed unnecessary plugins in Jenkins
    
    Co-authored-by: Aaron Miller <aaron.c.miller@saic.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -62,12 +62,15 @@ module "subnets" {
 }
 
 module "rke_rancher_master_cluster" {
-  source                          = "git::https://github.com/saic-oss/terraform-aws-rke-rancher-master-cluster.git?ref=tags/0.4.0"
+  source                          = "git::https://github.com/saic-oss/terraform-aws-rke-rancher-master-cluster.git?ref=tags/0.4.1"
   additional_tag_map              = {}
+  description                     = var.description
   instance_type                   = var.controlplane_instance_type
   kubernetes_version              = var.master_cluster_kubernetes_version
   name                            = var.name
   namespace                       = var.namespace
+  owner                           = var.owner
+  repo                            = var.repo
   node_group_1_subnet_id          = module.subnets.public_subnet_ids[0]
   node_group_2_subnet_id          = module.subnets.public_subnet_ids[1]
   node_group_3_subnet_id          = module.subnets.public_subnet_ids[2]
@@ -93,11 +96,14 @@ module "rke_rancher_master_cluster" {
 }
 
 module "rancher-k8s-cluster" {
-  source                     = "git::https://github.com/saic-oss/terraform-aws-rancher-k8s-cluster.git?ref=tags/0.4.6"
+  source                     = "git::https://github.com/saic-oss/terraform-aws-rancher-k8s-cluster.git?ref=tags/0.4.7"
   additional_tag_map         = {}
+  description                = var.description
   kubernetes_version         = var.worker_cluster_kubernetes_version
   name                       = "${var.name}-workload"
   namespace                  = var.namespace
+  owner                      = var.owner
+  repo                       = var.repo
   stage                      = var.stage
   region                     = var.region
   letsencrypt_email          = var.letsencrypt_email
